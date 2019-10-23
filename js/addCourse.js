f7b4ffd30d3173061871e855194a4f9c141c6527
charlesagyemang <micnkru@gmail.com>
2019-10-23 16:51:46 +0000

    Feat: Lawa

--- js/addCourse.js
+++ js/addCourse.js
@@ -1,4 +1,4 @@
-console.log("heyy from Add Course");
+// console.log("heyy from Add Course");
 
 // title
 // desc
@@ -50,3 +50,64 @@ $("#addNewCourse").unbind("click").click(function(){
   })
 })
 //===== Add Course Logic =====//
+
+
+//====== Add New Lesson Logic ====//
+$("#addNewLesson").unbind("click").click(function(){
+  $("#addLessonModalTitleText").html("Add New Lesson");
+  $("#addLessonSubmitButton").text("Add Lesson");
+
+
+  let coursesArray = [];
+  courses.forEach((eachCourse)=>{
+    const lam = `<option value=${eachCourse.id}>${eachCourse.title}</option>`;
+    coursesArray.push(lam);
+  })
+
+  // courseSelect
+  const finalSelectBody = `<select id="selectCourses">\n` + coursesArray.join("\n") + "\n</select>";
+
+  $("#courseSelect").html(finalSelectBody);
+
+  $("#addLessonSubmitButton").unbind("click").click(function(){
+    // get values
+    let $this = $(this);
+    const title = $("#inputLessonTitle").val();
+    const desc = $("#inputLessonDesc").val();
+    const youtubeLink = $("#inputLessonYoutubeLink").val();
+    const courseId = $('#selectCourses').find(":selected").val();
+    const channelId = channel.id;
+
+    if (title.length > 0){
+      const bodyToSend  = {
+        title: title,
+        desc: desc,
+        youtubeLink: youtubeLink,
+        courseId: courseId,
+        payload: {},
+      }
+
+      // console.log(channelId,bodyToSend);
+      $this.text("Creating Lesson....");
+
+      axios.post(`https://poole23.herokuapp.com/api/lessons/${channelId}`, bodyToSend, JSON.parse(localStorage.myConfig))
+      .then((response) => {
+        $this.text("Success...");
+        // console.log("...done");
+        console.log(response.data);
+        localStorage.channelGotten = JSON.stringify(response.data);
+        console.log(localStorage.gottenChannel);
+        location.reload();
+      }, (error) => {
+        console.log(error);
+      });
+
+      return;
+    }
+
+    alert("Error: Title Should Not Be Empty");
+
+
+  })
+
+})
